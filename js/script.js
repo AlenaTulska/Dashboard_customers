6df4c7dbf98618124191af0a687e695d499c3060
Alena Tulska <alona.tulska@khpi.edu.ua>
2024-10-11 16:24:43 +0300

    js added

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('.inact')
+btn.addEventListener('click', function() {
+  alert('This customer is inactive at the moment!');
+});
+
+const btnAct = document.querySelector('.act')
+btnAct.addEventListener('click', function() {
+  alert('Contact this customer!');
+});
+
+
